1495d24c442da875f081991147f63dbb540c5fd1
Melki Toldi <melchior.toldi@gmail.com>
2015-12-03 23:18:36 +0100

    select and css

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,32 @@
  $(function () {
         //Initialize Select2 Elements
-        $(".select2").select2();
+        d3.csv('static/data/dataV1.csv',function(e,data){
+        	//get all the keys
+
+        	var keys = data[0];
+        	var options = '';
+        	for(a in keys)
+        	{
+        		options+='<option id="'+a+'"">'+a+'</option>';
+        	
+        	}
+        	$(".select2#attributeOne").html(options);
+        	$(".select2").select2();
+
+        	$(".select2#attributeOne").on("change",function(){
+        		//from string to numeric
+        		data.forEach(function(d, i){
+        			d[this.value] = +d[this.value];
+        		});
+        		visualize(this.value);
+        	});	
+
+        	function visualize(idAttribute)
+        	{
+        		
+        	}
+        
+        });
+
+
 });
